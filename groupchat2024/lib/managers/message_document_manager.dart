e587c9af5742b5651b6f38d645076b0c0bcd1b6a
David Fisher <fisherds@rose-hulman.edu>
2024-11-07 13:45:29 -0500

    finished with the 6 managers

--- groupchat2024/lib/managers/message_document_manager.dart
+++ groupchat2024/lib/managers/message_document_manager.dart
@@ -1,66 +1,81 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:moviequotes/models/movie_quote.dart';
+import 'package:groupchat2024/models/group.dart';
+import 'package:groupchat2024/models/message.dart';
+import 'package:intl/intl.dart';
 
-class MovieQuoteDocumentManager {
-  MovieQuote? latestMovieQuote;
-  final CollectionReference _ref;
+class MessageDocumentManager {
+  Message? latestMessage;
+  String? _currentGroupDocumentId;
+  final CollectionReference _primaryRef;
 
-  static final MovieQuoteDocumentManager instance =
-      MovieQuoteDocumentManager._privateConstructor();
+  static final MessageDocumentManager instance =
+      MessageDocumentManager._privateConstructor();
 
-  MovieQuoteDocumentManager._privateConstructor()
-      : _ref =
-            FirebaseFirestore.instance.collection(kMovieQuotesCollectionPath);
+  MessageDocumentManager._privateConstructor()
+      : _primaryRef =
+            FirebaseFirestore.instance.collection(kGroupsCollectionPath);
+
+  CollectionReference get _currentMessagesColectionRef => _primaryRef
+      .doc(_currentGroupDocumentId)
+      .collection(kMessagesCollectionPath);
+
+  DocumentReference get _currentMessageRef =>
+      _currentMessagesColectionRef.doc(latestMessage!.documentId!);
 
   StreamSubscription startListening({
-    required String documentId,
+    required String groupDocumentId,
+    required String messageDocumentId,
     required Function observer,
-  }) =>
-      _ref.doc(documentId).snapshots().listen(
-          (DocumentSnapshot documentSnapshot) {
-        latestMovieQuote = MovieQuote.from(documentSnapshot);
-        observer();
-      }, onError: (error) {
-        print("Error listening for Movie Quote $error");
-      });
+  }) {
+    _currentGroupDocumentId = groupDocumentId;
+    return _currentMessagesColectionRef
+        .doc(messageDocumentId)
+        .snapshots()
+        .listen((DocumentSnapshot documentSnapshot) {
+      latestMessage = Message.from(documentSnapshot);
+      observer();
+    }, onError: (error) {
+      print("Error listening for Message $error");
+    });
+  }
 
   void stopListening(StreamSubscription? subscription) {
     subscription?.cancel();
   }
 
-  Future<void> delete() => _ref.doc(latestMovieQuote!.documentId!).delete();
+  Future<void> delete() => _currentMessageRef.delete();
 
-  // Comment out add, since update will have similarities.
-  Future<void> update({
-    required String quote,
-    required String movie,
-  }) {
-    return _ref.doc(latestMovieQuote!.documentId!).update({
-      kMovieQuoteQuote: quote,
-      kMovieQuoteMovie: movie,
-      kMovieQuoteLastTouched: Timestamp.now(),
+  Future<void> updateText(String text) {
+    return _currentMessageRef.update({
+      kMessageText: text,
     }).catchError((error) {
       print("There was an error: $error");
     });
   }
 
-  void restore(MovieQuote mqToRestore) {
-    _ref.doc(mqToRestore.documentId!).set({
-      kMovieQuoteQuote: mqToRestore.quote,
-      kMovieQuoteMovie: mqToRestore.movie,
-      kMovieQuoteLastTouched: mqToRestore.lastTouched,
+  void restore(Message messageToRestore) {
+    _currentMessagesColectionRef.doc(messageToRestore.documentId!).set({
+      kMessageText: messageToRestore.text,
+      kMessageAuthorEmail: messageToRestore.authorEmail,
+      kMessageCreated: messageToRestore.created,
     }).catchError((error) {
       print("There was an error: $error");
     });
   }
 
   void clearLatest() {
-    latestMovieQuote = null;
+    latestMessage = null;
   }
 
-  String get quote => latestMovieQuote?.quote ?? "";
-  String get movie => latestMovieQuote?.movie ?? "";
-  String get authorUid => latestMovieQuote?.authorUid ?? "";
+  String get text => latestMessage?.text ?? "";
+  String get authorEmail => latestMessage?.authorEmail ?? "";
+  String get createdString {
+    if (latestMessage != null) {
+      return "";
+    }
+    DateTime dateTime = latestMessage!.created.toDate();
+    return DateFormat("MM/dd/yyyy hh:mm a").format(dateTime);
+  }
 }
--- groupchat2024/lib/managers/messages_collection_manager.dart
+++ groupchat2024/lib/managers/messages_collection_manager.dart
@@ -1,46 +1,31 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:moviequotes/managers/auth_manager.dart';
-import 'package:moviequotes/models/movie_quote.dart';
+import 'package:groupchat2024/managers/auth_manager.dart';
+import 'package:groupchat2024/models/group.dart';
+import 'package:groupchat2024/models/message.dart';
 
-class MovieQuotesCollectionManager {
-  // Not needed if using Firebase UI Firestore
-  // var latestMovieQuotes = <MovieQuote>[];
-  final CollectionReference _ref;
+class MessageCollectionManager {
+  final CollectionReference _primaryRef;
+  String? _currentGroupDocumentId;
 
-  static final MovieQuotesCollectionManager instance =
-      MovieQuotesCollectionManager._privateConstructor();
+  static final MessageCollectionManager instance =
+      MessageCollectionManager._privateConstructor();
 
-  MovieQuotesCollectionManager._privateConstructor()
-      : _ref =
-            FirebaseFirestore.instance.collection(kMovieQuotesCollectionPath);
-
-// Not needed if using Firebase UI Firestore
-  // StreamSubscription startListening(Function observer) => _ref
-  //         .orderBy(kMovieQuoteLastTouched, descending: true)
-  //         .snapshots()
-  //         .listen((QuerySnapshot querySnapshot) {
-  //       latestMovieQuotes =
-  //           querySnapshot.docs.map((doc) => MovieQuote.from(doc)).toList();
-  //       observer();
-  //     }, onError: (error) {
-  //       print("Error listening for Movie Quotes $error");
-  //     });
-
-  // void stopListening(StreamSubscription? subscription) {
-  //   subscription?.cancel();
-  // }
+  MessageCollectionManager._privateConstructor()
+      : _primaryRef =
+            FirebaseFirestore.instance.collection(kGroupsCollectionPath);
 
   Future<void> add({
-    required String quote,
-    required String movie,
+    required String text,
   }) {
-    return _ref.add({
-      kMovieQuoteAuthorUid: AuthManager.instance.uid,
-      kMovieQuoteQuote: quote,
-      kMovieQuoteMovie: movie,
-      kMovieQuoteLastTouched: Timestamp.now(),
+    return _primaryRef
+        .doc(_currentGroupDocumentId)
+        .collection(kMessagesCollectionPath)
+        .add({
+      kMessageText: text,
+      kMessageAuthorEmail: AuthManager.instance.email,
+      kMessageCreated: Timestamp.now(),
     }).then((DocumentReference docRef) {
       print("The add is finished, the doc id was ${docRef.id}");
     }).catchError((error) {
@@ -48,13 +33,16 @@ class MovieQuotesCollectionManager {
     });
   }
 
-  Query<MovieQuote> get allMovieQuotesQuery =>
-      _ref.orderBy(kMovieQuoteLastTouched, descending: true).withConverter(
-            fromFirestore: (documentSnapshot, _) =>
-                MovieQuote.from(documentSnapshot),
-            toFirestore: (movieQuote, _) => movieQuote.toJsonMap(),
-          );
-
-  Query<MovieQuote> get onlyMyMovieQuotesQuery => allMovieQuotesQuery
-      .where(kMovieQuoteAuthorUid, isEqualTo: AuthManager.instance.uid);
+  Query<Message> allGroupMessagesQuery(String groupDocumentId) {
+    _currentGroupDocumentId = groupDocumentId; // Just to help add.
+    return _primaryRef
+        .doc(_currentGroupDocumentId)
+        .collection(kMessagesCollectionPath)
+        .orderBy(kMessageCreated)
+        .withConverter(
+          fromFirestore: (documentSnapshot, _) =>
+              Message.from(documentSnapshot),
+          toFirestore: (message, _) => message.toJsonMap(),
+        );
+  }
 }
