45aed52dd4b83052918d040530a3db58c713edc9
David Fisher <fisherds@rose-hulman.edu>
2024-11-08 13:29:54 -0500

    ready for my first real test

---
+++ groupchat2024/lib/pages/account/login_front_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:groupchat2024/components/square_button.dart';
+import 'package:groupchat2024/managers/auth_manager.dart';
+import 'package:groupchat2024/pages/account/register_new_user_page.dart';
+
+class LoginFrontPage extends StatefulWidget {
+  const LoginFrontPage({super.key});
+
+  @override
+  State<LoginFrontPage> createState() => _LoginFrontPageState();
+}
+
+class _LoginFrontPageState extends State<LoginFrontPage> {
+  UniqueKey? _loginObserverKey;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loginObserverKey = AuthManager.instance.addLoginObserver(() {
+      print("Login observed from the LoginFrontPage");
+      if (Navigator.of(context).canPop()) {
+        Navigator.of(context).pop();
+      }
+      // Navigator.of(context).push(
+      //   MaterialPageRoute(
+      //     builder: (context) => const GroupsListPage(),
+      //   ),
+      // );
+
+      // Navigator.of(context).popUntil((route) => route.isFirst);
+    });
+  }
+
+  @override
+  void dispose() {
+    AuthManager.instance.removeObserver(_loginObserverKey);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          const Expanded(
+            child: Center(
+              child: Text(
+                "Group Chat",
+                style: TextStyle(
+                  fontFamily: "Rowdies",
+                  fontSize: 56.0,
+                ),
+              ),
+            ),
+          ),
+          SquareButton(
+            displayText: "Log in",
+            onPressCallback: () {
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(
+              //     builder: (context) =>
+              //         const LoginExistingUserPage(),
+              //   ),
+              // );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have an account yet?"),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterNewUserPage(),
+                    ),
+                  );
+                },
+                child: const Text("Sign Up Here"),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 60.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ groupchat2024/lib/pages/account/register_new_user_page.dart
@@ -0,0 +1,155 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:groupchat2024/components/square_button.dart';
+import 'package:groupchat2024/managers/auth_manager.dart';
+import 'package:groupchat2024/managers/user_data_document_manager.dart';
+
+class RegisterNewUserPage extends StatefulWidget {
+  const RegisterNewUserPage({
+    super.key,
+  });
+
+  @override
+  State<RegisterNewUserPage> createState() => _RegisterNewUserPageState();
+}
+
+class _RegisterNewUserPageState extends State<RegisterNewUserPage> {
+  final _formKey = GlobalKey<FormState>();
+  final firstNameTextEditingController = TextEditingController();
+  final lastNameTextEditingController = TextEditingController();
+  final emailTextEditingController = TextEditingController();
+  final passwordTextEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // TODO: Remove this later
+    emailTextEditingController.text = "a@b.co";
+    passwordTextEditingController.text = "123456";
+  }
+
+  @override
+  void dispose() {
+    emailTextEditingController.dispose();
+    passwordTextEditingController.dispose();
+    firstNameTextEditingController.dispose();
+    lastNameTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Create a New User"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: firstNameTextEditingController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "First name can't be empty";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "First Name",
+                    hintText: "Enter a valid first name",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  controller: lastNameTextEditingController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Last Name",
+                    hintText: "Optional",
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                TextFormField(
+                  controller: emailTextEditingController,
+                  validator: (value) {
+                    if (value == null || !EmailValidator.validate(value)) {
+                      return "Invalid Email Address";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                    hintText: "Enter a valid email address",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  controller: passwordTextEditingController,
+                  validator: (value) {
+                    if (value == null || value.length < 6) {
+                      return "Passwords must be at least 6 characters";
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                    hintText: "Passwords must be 6 characters or more",
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                SquareButton(
+                  displayText: "Create an Account",
+                  onPressCallback: () {
+                    if (_formKey.currentState!.validate()) {
+                      AuthManager.instance.signInNewUser(
+                        context: context,
+                        emailAddress: emailTextEditingController.text,
+                        password: passwordTextEditingController.text,
+                      );
+                      UserDataDocumentManager.instance
+                          .createUserDataFromCurrentUser(
+                        firstName: firstNameTextEditingController.text,
+                        lastName: lastNameTextEditingController.text,
+                      );
+
+                      // AuthManager.instance.loginExistingUser(
+                      //   context: context,
+                      //   emailAddress: emailTextEditingController.text,
+                      //   password: passwordTextEditingController.text,
+                      // );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Invalid Email or Password"),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
